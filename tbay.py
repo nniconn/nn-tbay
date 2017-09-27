6ce065431b4e8c152f6331e7891f0ee96ec913a5
Nicole Nguyen <nicole.nguyen321@gmail.com>
2017-09-27 17:03:59 +0000

    Creating models SQLalchemy

---
+++ tbay.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, Float
+
+
+engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+
+class Item(Base):
+    __tablename__ = "items"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    start_time = Column(DateTime, default=datetime.utcnow)
+    
+    print('items table created')
+
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    
+    print('users table created')
+    
+class Bid(Base):
+    __tablename__ = "bids"
+    
+    id = Column(Integer, primary_key=True)
+    price = Column(Float, primary_key=True)
+    
+    print('bid table created')
+    
+    
+    
+    
+
+Base.metadata.create_all(engine)
